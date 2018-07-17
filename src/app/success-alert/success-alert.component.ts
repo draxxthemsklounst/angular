f0ffa8684bb09a603e35298a2cce5be2f8e1f480
monty <671montygim@gmail.com>
2018-07-17 12:07:31 +1000

    success and warning component

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-success-alert',
+    templateUrl: './success-alert.component.html',
+    styles: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit{
+    constructor () {}
+    ngOnInit() {
+
+    }
+}
