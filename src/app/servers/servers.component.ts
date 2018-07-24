559a7c1e7aca487522e51a66da403dbb054fe5d0
monty <671montygim@gmail.com>
2018-07-24 20:30:27 +1000

    Good Stuff

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,16 +5,31 @@ import { Component, OnInit } from '@angular/core';
   //below is for class types and attributes
   //selector: '[app-servers]',
   //selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'Server not created.';
+  serverName = 'Test Server'
+  ;
+  constructor() {
 
-  constructor() { }
+    setTimeout( () => {
+      this.allowNewServer = true;
+    },2000);//execute after 2 seconds
+
+   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Server name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
